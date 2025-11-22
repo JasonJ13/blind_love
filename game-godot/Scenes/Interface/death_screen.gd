50964c09878899d321961919423a424a6a970658
Khady <khady.diop0407@gmail.com>
2025-11-22 22:04:22 +0100

    Pause menu fonctionnel et death screen

--- game-godot/Scenes/Interface/death_screen.gd
+++ game-godot/Scenes/Interface/death_screen.gd
@@ -1,34 +1,26 @@
 extends CanvasLayer
 
-var active = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Echap") :
-		if active:
-			hide_menu()
-			active=false
-		else:
-			show_menu()
-			active = true
-	
-
-func hide_menu():
-	hide()
-
-func show_menu():
-	show()
-
-func _on_resume_pressed() -> void:
-	hide_menu()
-	active = false
+func _process(delta: float) -> void:
+	pass
 
 
-func _on_mainmenuback_pressed() -> void:
+func _on_retry_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_backmainmenu_pressed() -> void:
 	var tree = get_tree()
 	var cur_scene = tree.get_current_scene()
 	var packed_scene: PackedScene = load("res://Scenes/Interface/main_menu.tscn") 
