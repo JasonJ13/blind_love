dd6d163adb54010257fef973fb9b485ce42fcdbd
Célia D'Agostini <celidago57@gmail.com>
2025-11-23 11:54:45 +0100

    introoooo aie aie aie

--- game-godot/Scenes/Interface/death_screen.gd
+++ game-godot/Scenes/Interface/death_screen.gd
@@ -1,18 +1,10 @@
 extends CanvasLayer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func activate 
 
 func _on_retry_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().reload_current_scene()
 
 
 
