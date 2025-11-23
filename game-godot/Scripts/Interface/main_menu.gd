67e38123af4b6dde67d432a3d5d06930f49ed275
Ttl41 <no@titi.tsp>
2025-11-23 13:29:43 +0100

    Normalize SFX/Music + Add SFX for title screen

--- game-godot/Scripts/Interface/main_menu.gd
+++ game-godot/Scripts/Interface/main_menu.gd
@@ -4,6 +4,9 @@ signal start_game
 
 
 func _on_start_pressed() -> void:
+	$StartSound.play()
+	$AudioStreamPlayer.stop()
+	await get_tree().create_timer(0.5).timeout
 	start_game.emit()
 	hide()
 	process_mode = Node.PROCESS_MODE_DISABLED
